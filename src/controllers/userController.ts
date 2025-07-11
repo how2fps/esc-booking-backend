09c2ed4549902e4703513ec49ade912a39517154
how2fps <hearthstonesigh@gmail.com>
2025-07-11 13:13:59 +0800

    added get user by id and respective test

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -16,3 +16,37 @@ export const getAllUsers = async (req: Request, res: Response) => {
               });
        }
 };
+
+export const getUserById = async (req: Request, res: Response) => {
+       try {
+              const { id } = req.params;
+              const [rows] = await db.execute("SELECT id, name, email, phone_number, loyalty_points, created_at FROM users WHERE id = ?", [id]);
+              res.status(200).json({
+                     success: true,
+                     data: rows,
+              });
+       } catch (error) {
+              console.error("Error fetching users:", error);
+              res.status(500).json({
+                     success: false,
+                     message: "Error fetching users",
+              });
+       }
+};
+
+export const getUserByEmail = async (req: Request, res: Response) => {
+       try {
+              const { email } = req.params;
+              const [rows] = await db.execute("SELECT id, name, email, phone_number, loyalty_points, created_at FROM users WHERE email = ?", [email]);
+              res.status(200).json({
+                     success: true,
+                     data: rows,
+              });
+       } catch (error) {
+              console.error("Error fetching users:", error);
+              res.status(500).json({
+                     success: false,
+                     message: "Error fetching users",
+              });
+       }
+};
--- tests/controllers/user.test.ts
+++ tests/controllers/user.test.ts
@@ -1,4 +1,5 @@
 import express from "express";
+
 import request from "supertest";
 import pool from "../../src/db";
 import userRoutes from "../../src/routes/userRoutes";
@@ -16,7 +17,25 @@ describe("User Controller", () => {
                      expect(Array.isArray(response.body.data)).toBe(true);
               });
        });
+
+       describe("GET /api/users/:email", () => {
+              it("should return Alice Johnson", async () => {
+                     const response = await request(app).get("/api/users/alice.johnson@email.com").expect(200);
+                     expect(response.body).toHaveProperty("success", true);
+                     console.log(response.body);
+                     expect(response.body).toHaveProperty("data");
+                     expect(response.body.data[0]).toEqual({
+                            id: expect.any(Number),
+                            name: "Alice Johnson",
+                            email: "alice.johnson@email.com",
+                            phone_number: "+1-555-0101",
+                            loyalty_points: 120,
+                            created_at: expect.any(String),
+                     });
+              });
+       });
 });
+
 afterAll(async () => {
        await pool.end();
 });
