b84036fe48bc2c4e3e4d073e6d448e10162ebddf
janaleong <janaleong.sg@gmail.com>
2025-08-12 15:19:43 +0800

    enabled pollAllHotelPrices route

--- src/routes/hotelRoutes.ts
+++ src/routes/hotelRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAllHotels, getHotelById, getHotelPrices } from "../controllers/hotelController";
+import { getAllHotels, pollAllHotelPrices, getHotelById, getHotelPrices } from "../controllers/hotelController";
 
 const router = express.Router();
 
 router.get("/", getAllHotels);
-// router.get("/prices", pollAllHotelPrices);
+router.get("/prices", pollAllHotelPrices);
 router.get("/:id", getHotelById);
 router.get("/:id/prices", getHotelPrices);
 export default router;
